acc8643aa0f6011e9fc6b38535138f535c3a5aea
Pawel Srokosz <pawel.srokosz@cert.pl>
2019-01-08 13:09:37 +0100

    Added new options to new_command

--- dirt/commands/new.py
+++ dirt/commands/new.py
@@ -1,5 +1,7 @@
 import click
 
+from dateparser import parse
+
 from dirt.libs import incident, log
 
 from dirt import hooks
@@ -7,15 +9,18 @@ from dirt import hooks
 
 @click.command("new", help="Create new incident and set as current")
 @click.option("--desc", help="Short description of incident (optional)")
+@click.option("--created-at", help="Override creation date")
+@click.option("--owner", help="Override owner name")
 @click.pass_context
 @hooks.hookable
-def new_command(ctx, desc):
+def new_command(ctx, desc, created_at, owner):
     current = incident.get_current_incident()
     if not log.QUIET_MODE and current is not None and not current.closed:
         log.warning("Current incident {current} is opened.", current=current.identifier)
         if not click.confirm("Do you really want to create new incident?"):
             ctx.abort()
-    current = incident.Incident(cname=desc)
+    created_at = parse(created_at)
+    current = incident.Incident(cname=desc, created_on=created_at, owner=owner)
     current.store()
     incident.set_current_incident(current)
     log.success("New incident created {incident}", incident=current.identifier)
